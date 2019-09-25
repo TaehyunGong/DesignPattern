d5850d04884c7e08cb46c0129a681e0cf49a6de2
tony <tony950620@naver.com>
2019-09-25 17:03:36 +0900

    20190925 - Strategy Pattern

---
+++ DesignPattern/src/strategy/ItemVo.java
@@ -0,0 +1,43 @@
+package strategy;
+
+public class ItemVo {
+
+	private String itemNo;
+	private String itemName;
+	private int price;
+	
+	public ItemVo() {}
+	
+	public ItemVo(String itemNo, String itemName, int price) {
+		super();
+		this.itemNo = itemNo;
+		this.itemName = itemName;
+		this.price = price;
+	}
+
+	public String getItemNo() {
+		return itemNo;
+	}
+	public void setItemNo(String itemNo) {
+		this.itemNo = itemNo;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "ItemVo [itemNo=" + itemNo + ", itemName=" + itemName + ", price=" + price + "]";
+	}
+	
+	
+	
+}
